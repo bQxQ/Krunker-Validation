4818af51d5c559f35bf91b6426a792c5e2436e2b
#c <wackoalt@gmail.com>
2025-10-01 18:25:47 +0400

    Update token.py

--- token.py
+++ token.py
@@ -1,6 +1,6 @@
 import requests, json
 
-api='https://token-api67.vercel.app/cleanest/token' #free token ge
+api='https://token-api67.vercel.app/cleanest/token' #free token gen
 def get_token():
     print('req token...')
     try:
