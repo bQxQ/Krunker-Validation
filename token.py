c99202b3f5b5aa588a30d39a64cea4e11f501024
#c <wackoalt@gmail.com>
2025-10-04 22:13:15 +0400

    Update token.py

--- token.py
+++ token.py
@@ -1,20 +1,4 @@
-import requests, json
+import requests
 
-api='https://token-api67.vercel.app/cleanest/token' #free token gen
-def get_token():
-    print('req token...')
-    try:
-        r=requests.post(api,headers={'Content-Type':'application/json'},timeout=10)
-        d=r.json()
-        print(json.dumps(d,indent=2))
-        return d
-    except Exception as e:
-        print("Error:",e)
-        try: 
-            print("Status:",r.status_code,"Resp:",r.text)
-        except: pass
-        return None
-
-if __name__=='__main__':
-    t=get_token()
-    print("\nDone!" if t else "\nFail!")
+p = requests.get('https://token-api67.vercel.app/generate-token') #free token gen
+print(p.json())
