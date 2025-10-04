16650191afaf109647357092217aad7961fe4cb6
#c <wackoalt@gmail.com>
2025-10-04 22:37:07 +0400

    Update token.py

--- token.py
+++ token.py
@@ -1,4 +1,6 @@
 import requests
 
-p = requests.get('https://token-api67.vercel.app/generate-token') #free token gen
+
+# If you want free access to the api dm @cleanest on discord
+p = requests.get('https://token-api67.vercel.app/generate-token') 
 print(p.json())
